b7c7ca6095824190de36a5a4762499962bd740ec
adin3012 <84287719+adin3012@users.noreply.github.com>
2022-08-06 20:46:06 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,18 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navList=document.querySelector('.nav-list')
+rightnav=document.querySelector('.rightnav')
+
+
+
+
+burger.addEventListener('click',()=>{
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+    rightnav.classList.toggle('v-class-resp');
+
+
+  
+    
+   
+})
